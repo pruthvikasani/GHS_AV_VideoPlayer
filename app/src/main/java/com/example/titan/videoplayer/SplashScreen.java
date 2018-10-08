e3f30a600c80299e4cf684546c7b2f8918c3c91b
Pruthvi Kasani <pkasani1@gmail.com>
2018-10-08 15:18:14 +0530

    App logo and Splash screen

--- app/src/main/java/com/example/titan/videoplayer/SplashScreen.java
+++ app/src/main/java/com/example/titan/videoplayer/SplashScreen.java
@@ -2,27 +2,41 @@ package com.example.titan.videoplayer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int time=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Thread myThread = new Thread(){
+//        Thread myThread = new Thread(){
+//            @Override
+//            public void run() {
+//                try {
+//                    sleep(500);
+//                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+//                    finish();
+//
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//        myThread.start();
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    sleep(500);
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
             }
-        };
-        myThread.start();
+        },time);
     }
 }
